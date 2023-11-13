52f2a63cced040370b6c508c3d8909e4661c5d8c
Ana Morales <anab_morales@hotmail.com>
2023-11-13 21:37:52 +0100

    añado comentario en código

---
+++ src/main/java/com/mycompany/bucles_do_while/Bucles_do_while.java
@@ -0,0 +1,17 @@
+package com.mycompany.bucles_do_while;
+
+/*creo un comentario
+de dos líneas */
+import java.util.Scanner;
+public class Bucles_do_while {
+
+public static void main(String[] args){
+Scanner an = new Scanner(System.in);
+int numero;
+do{
+System.out.println("Escribe un número");
+numero = an.nextInt();
+}while( !(numero >1 && numero <=10));
+System.out.println("has escrito "+numero);
+}
+}
